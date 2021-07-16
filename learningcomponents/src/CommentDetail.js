21df6109d9d07c5e1baeea5d4f2281477ff6ef2e
ayuti98 <ayuti@sparkplustech.in>
2021-07-17 01:57:08 +0530

    setting props and customizing name using props

--- learningcomponents/src/CommentDetail.js
+++ learningcomponents/src/CommentDetail.js
@@ -1,6 +1,10 @@
 import React  from 'react';
 import faker from 'faker';
-const CommentDetail = () => {
+
+// so to get access of the props that provided to the p component that prop is going to show inside of a object
+//that is provided as the first argument to the function.
+const CommentDetail = (props) => {
+    // console.log(props);
     return (
         <div className="comment">
         <a href="/" className="avatar">
@@ -8,7 +12,11 @@ const CommentDetail = () => {
         </a>
         <div className="content">
             <a href="/" className="author" >
-                Sam
+               {props.author}
+               {/* replace the hardcoded value with {} about to reference javascript variable and
+               then inside that i reference props.author, which means whenever this CommentDetail is
+               shown on the screen CommentDetail is going to look at the props object and will check if there is author
+               property in there and that is going to take that author name and stick it into the anchor tag */}
             </a>
             <div className="metadata">
                 <span className="date">Today at 6.00PM</span>
--- learningcomponents/src/index.js
+++ learningcomponents/src/index.js
@@ -4,8 +4,12 @@ import CommentDetail from './CommentDetail';
 const App = () => {
     return (
         <div className="ui container comments">
-           
-<CommentDetail/>
+            {/* whenever we provide props to the component is always going to be unique version of that component, for eg
+            the author="sam" is not going to be same like other props*/ }
+       <CommentDetail author="Sam" />
+       <CommentDetail author="Alex"/>
+       <CommentDetail author="Jane"/>
+      
         </div>
     );
 };
